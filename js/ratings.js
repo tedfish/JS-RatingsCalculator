907a152c4c5b37732ad9ce5a84a0cd44de051b37
TDN <tedfish@gmail.com>
2022-01-04 10:36:39 -0800

    check

---
+++ js/ratings.js
@@ -0,0 +1,8 @@
+function collect_ratings() {
+  const ratings = { count: 0, sum: 0, average: 0 }
+  let rating = 0
+  let elements = document.querySelectorAll(".rating")
+  elements.forEach((element)=>{
+    element.id.replace()
+  })
+}
